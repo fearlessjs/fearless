f294856185e5069d7c40662c12884629069117d4
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-08 21:06:56 -0300

    improve index

--- src/index.js
+++ src/index.js
@@ -4,57 +4,17 @@ const uWebSockets = require('uWebSockets.js')
 // const ssl = require('./ssl')
 // const options = require('./options')
 
-const port = 3000
-
-uWebSockets
-  .App({
+const app = ({ key, cert, passphrase }) =>
+  uWebSockets.App({
     // key_file_name: 'misc/key.pem',
-    // cert_file_name: 'misc/cert.pem', 
+    // cert_file_name: 'misc/cert.pem',
     // passphrase: '1234'
   })
-  .get('/', (res, _req) => {
-    res.end('Hello World!')
-  })
-  .get('/sign-in', (res, _req) => {
-    res.end('SIGN-IN!')
-  })
-  .any('/*', (res, _req) => {
-    res.end('Nothing to see here!')
-  })
-  .listen(port, token => {
-    if (token) {
-      console.log('Listening to port ' + port)
-    } else {
-      console.log('Failed to listen to port ' + port)
-    }
-  })
 
-// uWebSockets
-//   .SSLApp(ssl)
-//   .ws('/*', {
-//     ...options,
-//     /* Handlers */
-//     open: (ws, req) => {
-//       console.log('A WebSocket connected via URL: ' + req.getUrl() + '!')
-//     },
-//     message: (ws, message, isBinary) => {
-//       /* Ok is false if backpressure was built up, wait for drain */
-//       let ok = ws.send(message, isBinary)
-//     },
-//     drain: ws => {
-//       console.log('WebSocket backpressure: ' + ws.getBufferedAmount())
-//     },
-//     close: (ws, code, message) => {
-//       console.log('WebSocket closed')
-//     }
-//   })
-//   .any('/*', (res, req) => {
-//     res.end('Nothing to see here!')
-//   })
-//   .listen(port, token => {
-//     if (token) {
-//       console.log('Listening to port ' + port)
-//     } else {
-//       console.log('Failed to listen to port ' + port)
-//     }
-//   })
+// require('./routes')(app)
+
+const get = (path, callback) => app({}).get(path, callback)
+
+module.exports = { app, get }
+module.exports.ramdaless = app
+module.exports.get = get

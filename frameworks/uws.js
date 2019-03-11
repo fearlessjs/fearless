6cfb227994a95447180095e8a55da028cae6754f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-11 13:53:39 -0300

    add new endpoints in uws

--- frameworks/uws.js
+++ frameworks/uws.js
@@ -5,4 +5,12 @@ uws
   .get('/', (res, req) => {
     res.end('Hello, World!')
   })
+  .get('/small', (res, req) => {
+    const small = require('../jsons/example1.json')
+    res.end(JSON.stringify(small))
+  })
+  .get('/big', (res, req) => {
+    const big = require('../jsons/example2.json')
+    res.end(JSON.stringify(big))
+  })
   .listen(3000, () => {})

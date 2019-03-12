7df2c3e2ea791750a5f5dbae00e69044842f5d7f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-12 02:11:20 -0300

    improve cors

--- cors/index.js
+++ cors/index.js
@@ -1,5 +1,34 @@
-module.exports = (configs = {}) => handlers => ({
-  configs: {
+const DEFAULT_ALLOW_METHODS = [
+  'POST',
+  'GET',
+  'PUT',
+  'PATCH',
+  'DELETE',
+  'OPTIONS'
+]
+
+const DEFAULT_ALLOW_HEADERS = [
+  'X-Requested-With',
+  'Access-Control-Allow-Origin',
+  'X-HTTP-Method-Override',
+  'Content-Type',
+  'Authorization',
+  'Accept'
+]
+
+const DEFAULT_MAX_AGE_SECONDS = 60 * 60 * 24
+
+module.exports = (
+  options = {
+    origin: '*',
+    maxAge: DEFAULT_MAX_AGE_SECONDS,
+    allowMethods: DEFAULT_ALLOW_METHODS,
+    allowHeaders: DEFAULT_ALLOW_HEADERS,
+    allowCredentials: true,
+    exposeHeaders: []
+  }
+) => handlers => ({
+  cors: {
     origin: ['*']
   },
   handlers

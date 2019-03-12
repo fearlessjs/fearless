a735ca48a4e48947ca09a85b60b4a7ad1ffa1fcc
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-11 22:57:12 -0300

    improve example frameworks/ramdaless.js

--- frameworks/ramdaless.js
+++ frameworks/ramdaless.js
@@ -1,7 +1,7 @@
 const { ramdaless, get } = require('../src/index')
 
-const helloWorld = get('/', (res, req) => {
-  res.end('Hello, World!')
+const helloWorld = get('/*', (res, req) => {
+  res.end('HELLO WORLD')
 })
 
 ramdaless({

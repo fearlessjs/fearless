cd68b121f4a3754004fd08a6c1a0733f2cb75801
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-14 00:11:41 -0300

    remove unused codes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 'use strict'
 
 const uWS = require('uWebSockets.js')
-const { equals, map, type, includes, ...rest } = require('ramda')
-const { stringify, parseJSON, isValidJSON } = require('./json')
 const { getSSLDefault, getListenDefault, getOptions } = require('./utils')
 
 const http = require('./http')
 
-const fearless = (options) => {
+const fearless = options => {
   const { cors, ssl, handlers, listen } = getOptions(options)
 
   console.log(JSON.stringify({ cors, ssl, handlers }, null, 4))
 
   try {
     const app = uWS.App(getSSLDefault(ssl))
-    map(http.setHandler(app), handlers)
+    handlers.forEach(http.setHandler(app))
     app.listen(listen.port, getListenDefault(listen))
   } catch (error) {
     console.error(error)
@@ -23,12 +21,5 @@ const fearless = (options) => {
 
 module.exports = {
   fearless,
-  ...http,
-  parseJSON,
-  isValidJSON,
-  stringify,
-  ...rest,
-  equals,
-  map,
-  type
+  ...http
 }

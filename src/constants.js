155a99644f6e6eaf9c3a52c75c6de590ee63de69
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-14 01:22:19 -0300

    improve src/constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
 'use strict'
 
 const HTTP = Object.freeze({
-  GET: 'GET',
-  POST: 'POST',
-  PUT: 'PUT',
-  DEL: 'DEL',
-  OPTIONS: 'OPTIONS',
-  PATCH: 'PATCH',
-  HEAD: 'HEAD',
-  CONNECT: 'CONNECT',
-  TRACE: 'TRACE',
-  ANY: 'ANY',
-  WEB_SOCKET: 'WEB_SOCKET'
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DEL: 'del',
+  OPTIONS: 'options',
+  PATCH: 'patch',
+  HEAD: 'head',
+  CONNECT: 'connect',
+  TRACE: 'trace',
+  ANY: 'any',
+  WEB_SOCKET: 'web_socket'
 })
 
 module.exports.HTTP = HTTP

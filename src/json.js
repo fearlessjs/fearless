eef447aa37bf7ed58bb1d5a21a77d56a0ba99aed
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-14 22:31:32 -0300

    refactor json

--- src/json.js
+++ src/json.js
@@ -1,34 +1,38 @@
-const json = res => {
+const json = (res) => {
   let buffer
 
-  return new Promise((resolve, reject) => {
-    res.onData((ab, isLast) => {
-      let chunk = Buffer.from(ab)
-      if (isLast) {
-        let json
-        if (buffer) {
-          try {
-            json = JSON.parse(Buffer.concat([buffer, chunk]))
-          } catch (e) {
-            reject(e)
-          }
-          resolve(json)
-        } else {
-          try {
-            json = JSON.parse(chunk)
-          } catch (e) {
-            reject(e)
-          }
-          resolve(json)
+  res.onData((ab, isLast) => {
+    let chunk = Buffer.from(ab)
+    if (isLast) {
+      let json
+      if (buffer) {
+        try {
+          json = JSON.parse(Buffer.concat([buffer, chunk]))
+        } catch (e) {
+          res.close()
+          return
         }
+        return json
       } else {
-        if (buffer) {
-          buffer = Buffer.concat([buffer, chunk])
-        } else {
-          buffer = Buffer.concat([chunk])
+        try {
+          json = JSON.parse(chunk)
+        } catch (e) {
+          res.close()
+          return
         }
+        return json
       }
-    })
+    } else {
+      if (buffer) {
+        buffer = Buffer.concat([buffer, chunk])
+      } else {
+        buffer = Buffer.concat([chunk])
+      }
+    }
+  })
+
+  res.onAborted(() => {
+    console.error('JSON invalid')
   })
 }
 

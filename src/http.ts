edf24c6ed59a166c5b09a305acd794a396e36678
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-04-09 00:41:22 -0300

    add typescript

--- src/http.ts
+++ src/http.ts
@@ -1,7 +1,26 @@
-'use strict'
+const HTTP = Object.freeze({
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DEL: 'del',
+  OPTIONS: 'options',
+  PATCH: 'patch',
+  HEAD: 'head',
+  CONNECT: 'connect',
+  TRACE: 'trace',
+  ANY: 'any',
+  WEB_SOCKET: 'web_socket'
+})
 
-const { getMethods } = require('./utils')
-const { HTTP } = require('./constants')
+const getMethods = method =>
+  method === HTTP.WEB_SOCKET
+    ? (pattern, options, handlers) => ({
+      method,
+      pattern,
+      options,
+      handlers
+    })
+    : (pattern, handler) => ({ method, pattern, handler })
 
 const [
   get,

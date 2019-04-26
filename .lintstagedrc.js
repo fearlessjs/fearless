2652278f45795dd7a54ec82c73bfab2c7823a4a1
Oler <roodrigoprogrammer@gmail.com>
2019-04-26 18:51:38 -0300

    rename .js to .json

--- .lintstagedrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  '*.js': ['yarn standard', 'git add']
-}
--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    parser: 'typescript',
-    printWidth: 80,
-    semi: false,
-    singleQuote: true,
-    tabWidth: 2,
-    trailingComma: 'all',
-  }
